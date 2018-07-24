423d601a2fc7225eb7691cb515a151fe1c31adee
Michael Heiberger <michael.heiberger@gmail.com>
2018-07-24 15:14:29 -0700

    Forgot to update the dbInit script to actually point to the Heroku postgreSQL db

--- dbInit.js
+++ dbInit.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('postgres://postgres:miy@motoS120@localhost:5432/get-hype-bot', {
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres'
 });
 
