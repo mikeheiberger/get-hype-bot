ef1a08eb912a96f1e15f2295587b6b12a1dafbdb
Mike Heiberger <michael.heiberger@gmail.com>
2018-09-15 17:52:07 -0700

    Added a new sound type as part of a migration strategy

---
+++ models/sounds_new.js
@@ -0,0 +1,28 @@
+module.exports = (sequelize, DataTypes) => {
+    return sequelize.define('sounds_new', {
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        link: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        duration: {
+            type: DataTypes.INTEGER,
+            default: -1
+        },
+        start: {
+            type: DataTypes.INTEGER,
+            default: 0
+        },
+        volume: {
+            type: DataTypes.INTEGER,
+            default: 1,
+        },
+        server: {
+            type: DataTypes.STRING
+        }
+    });
+};
