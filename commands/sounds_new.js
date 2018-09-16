5d652536411d8cb5a4610703727cccf12d84ea47
Mike Heiberger <michael.heiberger@gmail.com>
2018-09-16 15:30:44 -0700

    Added displaying sounds by server (or not server)

--- commands/sounds_new.js
+++ commands/sounds_new.js
@@ -5,20 +5,20 @@ module.exports = {
     description: 'Lists the current sounds available to play (from new data type)',
     cooldown: 30,
     async execute(message, args) {
-        const sounds = await SoundsNew.findAll({
-            where: {
-                $or: [
-                    { server: message.guild.id },
-                    { server: null }
-                ]
-            },
+        const soundsAll = await SoundsNew.findAll({
+            where: { server: null },
             attributes: ['name']
         });
 
-        if (!sounds) {
+        const soundsServer = await SoundsNew.findAll({
+            where: { server: message.guild.id },
+            attributes: ['name']
+        });
+
+        if (!soundsAll && !soundsServer) {
             return message.reply('no sounds are currently in the database. Add one!');
         }
 
-        return message.reply(`here are the current sounds you can play:\n${sounds.map(sound => sound.name).join('\n')}`);
+        return message.reply(`here are the current sounds you can play.\n\n*Sounds for everyone:*\n${soundsAll.map(sound => sound.name).join('\n')}\n\n*Server specific sounds:*\n${soundsServer.map(sound => sound.name).join('\n')}\n  `);
     }
 };
